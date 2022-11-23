a209b77a394e9bf9580c427820cc250c6374e765
s1290029 <s1290029@u-aizu.ac.jp>
2022-11-24 00:07:31 +0900

    Create Recipe of lisp
    Add how to comment, calculate, print and scan.

---
+++ recipe.lisp
@@ -0,0 +1,73 @@
+;;This is recipe of lisp program
+
+;;comment
+#|
+  comment
+|#
+
+;;calculate
+#|
+(+ 1783 813)
+(- 1783 813)
+(* 1783 813)
+(/ 1783 813)
+(expt 2 5)
+|#
+
+;;declaration
+
+;;print
+#|
+(print "Hello World")
+
+(format t "Hello World")
+
+(format nil "I like ~a" 8)
+
+(format t "~$" pi)
+
+(format t "~d" 1783813)
+
+(format t "~:d" 1783813)
+
+(format t "~@d" 1783813)
+
+(format nil "~b" 1783)
+
+(format nil "~o" 1783)
+
+(format nil "~x" 1783)
+
+(format nil "~r" 1783)
+
+(format nil "~(~a~)" "HELLO")
+|#
+
+;;scan
+#|
+(read-line var)
+
+(open "c:\\Users\\fujitihi\\lisp\\tutorial\\sample.txt")
+
+(let ((in (open "c:\\Users\\fujitihi\\lisp\\tutorial\\sample.txt" :if-does-not-exist nil)))
+(format t "~a~%" (read-line in))
+(close in))
+
+(let ((in (open "c:\\Users\\fujitihi\\lisp\\tutorial\\sample.txt" :if-does-not-exist nil)))
+(when in
+(loop for line = (read-line in nil)
+while line do (format t "~a~%" line))
+(close in)))
+
+(with-open-file (stream "c:\\Users\\fujitihi\\lisp\\tutorial\\sample.txt")
+(format t "~a~%" (read-line stream)))
+
+(with-open-file (stream "c:\\Users\\fujitihi\\lisp\\tutorial\\test.txt" :direction :output :if-exists :supersede)
+(format stream "I wrote this sentence in tutorial.lisp!"))
+|#
+
+;;and or not
+
+;;if
+
+;;loop
